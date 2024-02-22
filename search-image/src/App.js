38fd5c67779da542f83b6f083ff96fd3d6beb547
carrymycross7 <threet.ldustin@gmail.com>
2024-02-21 20:41:47 -0600

    add state to searchBar, add term searching

--- search-image/src/App.js
+++ search-image/src/App.js
@@ -1,13 +1,17 @@
 import SearchBar from './components/SearchBar';
-
+import ImageList from "./components/ImageList";
+import searchImages from "./api";
 function App() {
-  const handleSubmit = (term) => {
-    console.log('Do a search with', term);
+  const handleSubmit = async (term) => {
+    const imageResults = await searchImages(term);
+
+    console.log(imageResults); // debug - remove
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList />
     </div>
   );
 }
