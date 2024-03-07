9de38cb85601ba70b12612b84ca7defb5fb07896
carrymycross7 <threet.ldustin@gmail.com>
2024-03-06 20:57:02 -0600

    add book create function

--- booksV1/src/App.js
+++ booksV1/src/App.js
@@ -4,8 +4,12 @@ import BookCreate from './components/BookCreate';
 function App() {
   const [books, setBooks] = useState([]);
 
+
   const createBook = (title) => {
-    console.log('Need to add book with:', title);
+    const updatedBooks = [
+        ...books,{id: 123, title}
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
