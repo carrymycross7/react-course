8af91a1e50613ab2a4d65523f0babcab56bd0995
carrymycross7 <threet.ldustin@gmail.com>
2024-03-14 20:28:39 -0500

    add book delete and book edit

--- booksV1/src/components/BookCreate.js
+++ booksV1/src/components/BookCreate.js
@@ -14,7 +14,7 @@ function BookCreate({ onCreate }) {
   };
 
   return (
-    <div className='book-create'>
+    <div className="book-create">
       <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
         <label>Title</label>
